b01c0375dac7b89efd33ece6b67d902e7f67fcd2
Anthony Good <anthony@quipper.com>
2017-03-21 19:05:59 +0000

    remove redux-related files

--- src/app/action_types.js
+++
@@ -1,4 +0,0 @@
-export const SEARCH_BEGIN   = 'SEARCH_BEGIN'
-export const SEARCH_SUCCESS = 'SEARCH_SUCCESS'
-export const SEARCH_ERROR   = 'SEARCH_ERROR'
-export const SELECT_MOVIE   = 'SELECT_MOVIE'
